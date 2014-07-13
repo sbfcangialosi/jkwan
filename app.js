3a7a276dade6cc4003ee127a1c34ea07434a6a44
Frank Cangialosi <fcangialosi94@gmail.com>
2014-07-13 12:58:14 -0400

    manually fetching twitter and instagram server-side, and sending to client

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ var express = require('express'),
 	stylus = require('stylus'),
 	nib = require('nib'),
 	fs = require('fs'),
-	request = require('request');
+	request = require('request'),
+	util = require('util'),
+	Twit = require('twit');
 
 var app = express();
+
+var T = new Twit({
+	consumer_key: 'mr87LvmetQRQg9gR7IPDPexWX',
+	consumer_secret: 'iAETAqN6RurKv1VWeS68vegvLbMfs1tFh3LYQbE2n5qQ4VELRC',
+	access_token: '2625256832-686SDrJGW9IPstSPQScHZPvAl9vNzAindSgLHQE',
+	access_token_secret: '6OThS6l2NYBEs4QFIcnawSucjWlEcujLKyRFTFECirzP1'
+});
 
 app.set('view engine', 'jade'); //templates located in /views/*.jade
 app.set('case sensitive routing', 'true');
@@ -34,6 +43,8 @@ app.use(express.static(__dirname, 'public'));
 
 app.get('/', function(req,res) {
 	photos = []
+	tweets = []
+	counter = 0
 	request('https://api.instagram.com/v1/users/23362758/media/recent?client_id=c836878d8188457799e29b06d9205263&count=5', function (err, resp, body) {
 		if (!err && resp.statusCode == 200) {
 			instagram = JSON.parse(body);
@@ -41,7 +52,21 @@ app.get('/', function(req,res) {
 				photos.push(String(item['images']['thumbnail']['url']));
 			});
 		}
-		res.render('index', {'photos' : photos});
+		T.get('/statuses/user_timeline', 
+			{screen_name:  'eternallyjackie', 
+			count: 5},
+			function(err, data, resp) {
+				if(!err && resp.statusCode == 200) {
+					tweets = data;
+					tweets.forEach(function(tweet) {
+						console.log(tweet['text']);
+						counter+=1;
+						tweets.push(tweet['text'])
+					});
+				}
+				console.log(counter);
+				res.render('index', {'photos' : photos, 'tweets' : tweets});
+			});
 	});
 	
 });
